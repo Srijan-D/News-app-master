d4f1377331a4dd8623e56474beae933ccd1c93da
srijan <srijandubey3105@gmail.com>
2023-04-22 22:22:38 +0530

    spreading and testing news response received via graphql endpoint

--- lib/sortNewsByImage.ts
+++ lib/sortNewsByImage.ts
@@ -1,12 +1,20 @@
-
 function sortNewsByImage(news: NewsResponse) {
-const newsWithImages= news.data.filter((item)=>item.image!==null)
-const newsWithoutImages= news.data.filter((item)=>item.image===null)
+    const newsWithImages = news.data.filter((item) => item.image !== null)
+    const newsWithoutImages = news.data.filter((item) => item.image === null)
 
+    // //either we spread or we riginalspread
 
-    return (
-     
-    )
+    // const newsWithImagesAndText = [...newsWithImages, ...newsWithoutImages]
+
+    //or we spread
+
+    const sortedNews = {
+        pagination: news.pagination,
+        data: [...newsWithImages, ...newsWithoutImages]
+    }
+
+    return sortedNews
+
 }
 
 export default sortNewsByImage
