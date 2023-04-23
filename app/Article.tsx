c6dcc106f0ff16774fc5817f75acadcebce55a09
srijan <srijandubey3105@gmail.com>
2023-04-23 20:15:55 +0530

    adding tailwind plugin line-clamp

--- app/Article.tsx
+++ app/Article.tsx
@@ -6,19 +6,19 @@ type Props = {
 
 function Article({ article }: Props) {
   return (
-    <article>
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out ">
       {article.image && (
         <img 
         src={article.image} 
         alt={article.title}
-        className="h-56 w-full object-cover shadow-md"
+        className="h-56 w-full object-cover shadow-md rounded-t-lg"
         />
       )}
-      <div>
-        <div>
-          <h2>{article.title}</h2>
-          <section>
-            <p>{article.description}</p>
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col p-5 ">
+          <h2 className="font-bold font-serif ">{article.title}</h2>
+          <section className="mt-2 flex-1">
+            <p className="text-xs line-clamp-6">{article.description}</p>
           </section>
           <footer>
             <p>{article.source}</p>
