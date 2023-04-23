e285d59ad3bedbc97b3cc1ba73cd3a111b88ec30
srijan <srijandubey3105@gmail.com>
2023-04-23 20:02:35 +0530

    adding data from stepzen removing Image from next as we domain is not known we cant whitelist everything

--- app/Article.tsx
+++ app/Article.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
+import Image from "next/image"
 
 type Props = {
   article: Article
 }
 
-function Article({article}: Props) {
+function Article({ article }: Props) {
   return (
     <article>
-      {article.image&&(
-        
+      {article.image && (
+        <img 
+        src={article.image} 
+        alt={article.title}
+        className="h-56 w-full object-cover shadow-md"
+        />
       )}
+      <div>
+        <div>
+          <h2>{article.title}</h2>
+          <section>
+            <p>{article.description}</p>
+          </section>
+          <footer>
+            <p>{article.source}</p>
+            <p>{article.published_at}</p>
+          </footer>
+        </div>
+      </div>
     </article>
   )
 }
