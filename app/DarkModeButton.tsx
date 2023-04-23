445a8e78c210eafe2d5e7dca409593ee59e9745f
srijan <srijandubey3105@gmail.com>
2023-04-24 01:52:52 +0530

    implemented Light Dark mode using next-theme

--- app/DarkModeButton.tsx
+++ app/DarkModeButton.tsx
@@ -1,14 +1,36 @@
 "use client"
 import { useTheme } from "next-themes";
 import { useState, useEffect } from "react"
-
-
-
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid"
 
 function DarkModeButton() {
-    return (
-        <div>DarkModeButton</div>
-    )
+    const [mounted, setMounted] = useState(false);
+    const { systemTheme, theme, setTheme } = useTheme();
+
+    //server does not know the current theme of the user hence it should only run once in the DOM
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+
+    if (!mounted) return null;
+
+    const currentTheme = theme === 'system' ? systemTheme : theme;
+
+    return <div>
+        {
+            currentTheme === 'dark' ? (
+                <SunIcon
+                    className="h-8 w-8 cursor-pointer text-yellow-500"
+                    onClick={() => setTheme('light')} />
+            ) : (
+                    <MoonIcon
+                        className="h-8 w-8 cursor-pointer text-gray-900"
+                        onClick={() => setTheme('dark')} />
+                )}
+
+    </div>
+
 }
 
 export default DarkModeButton
