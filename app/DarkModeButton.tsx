6681ccb48fec916b8d18141236b358a457ef8435
srijan <srijandubey3105@gmail.com>
2023-04-23 21:05:36 +0530

    adding dark mode with themes

---
+++ app/DarkModeButton.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react"
+
+
+
+
+function DarkModeButton() {
+    return (
+        <div>DarkModeButton</div>
+    )
+}
+
+export default DarkModeButton
