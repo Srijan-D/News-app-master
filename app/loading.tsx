9bc82292dd4951099ef22e082b9b2058c78d8c51
srijan <srijandubey3105@gmail.com>
2023-04-25 01:03:23 +0530

    custom loader in next(uses react suspense under the hood)

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+function Loading({}: Props) {
+  return (
+    <div className="animate-pulse font-serif text-lg text-gray-400 text-center p-10">Loading News Feed...</div>
+  )
+}
+
+export default Loading
