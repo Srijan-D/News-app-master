107c1d381c1d1870dde6fe7e6cf823cfa2280d39
srijan <srijandubey3105@gmail.com>
2023-04-24 17:23:01 +0530

    adding custom title class under componenets directive

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -14,7 +14,7 @@ function ArticlePage({ searchParams }: Props) {
 
     return (
         <article>
-            <section>
+            <section className="flex flex-col lg:flex-row px-0 lg:px-10">
                 {article.image && (
                     <img src={article.image} alt="article.title"
                         className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl
@@ -23,11 +23,13 @@ function ArticlePage({ searchParams }: Props) {
                     />
                 )}
                 <div className="px-10">
-                    <h1 className="px-0 no-underline pb-2">{article.title}</h1>
-                    <div>
-                        <h2>By: {(article.author !== null) ? article.author : "unknown"}</h2>
-                        <h2>Source: {(article.source !== null) ? article.source : "unknown"}</h2>
+                    <h1 className="Title px-0 no-underline pb-2">{article.title}</h1>
+                    <div className="flex divide-x-2 space-x-4">
+                        <h2 className="font-bold">By: {(article.author !== null) ? article.author : "unknown"}</h2>
+                        <h2 className="font-bold pl-4">Source: {(article.source !== null) ? article.source : "unknown"}</h2>
+                        <p className="pl-4">{article.published_at}</p>
                     </div>
+                    <p className="pt-4">{article.description}</p>
                 </div>
             </section>
 
